cba32b8976b1dc593c5aa37ed122393819caae2f
iftekharweb <iftekharweb@gmail.com>
2024-04-10 15:14:55 +0600

    permissions added [except marks]

--- backend/courses/views.py
+++ backend/courses/views.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Course
+from .permissions import IsSystemAdminOrReadOnly
 from .serializers import CourseSerializer
 
 class CourseViewSet(ModelViewSet):
+    permission_classes = [IsSystemAdminOrReadOnly]
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
